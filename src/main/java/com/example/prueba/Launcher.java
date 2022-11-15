94835f9be11935fe533a4f7cadf37f62d05eff35
unknown <alexander.baikalov@inslapineda.cat>
2022-11-15 09:00:02 +0000

    crear un ejecutable jar

---
+++ src/main/java/com/example/prueba/Launcher.java
@@ -0,0 +1,9 @@
+package com.example.prueba;
+
+
+    public class Launcher {
+        public static void main(String[] args) {
+            Main.main(args);
+        }
+    }
+
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,7 @@
 module com.example.prueba {
     requires javafx.controls;
     requires javafx.fxml;
-    requires com.jfoenix;
     requires java.sql;
-    requires mysql.connector.java;
 
     opens com.example.prueba to javafx.fxml;
     exports com.example.prueba;
